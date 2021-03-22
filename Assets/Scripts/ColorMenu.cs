f83891e5ee8c363849b76a5319be806301f1250a
GreenNBlacko <45263750+GreenNBlacko@users.noreply.github.com>
2021-03-22 18:06:11 +0200

    Add a color preset menu

--- Assets/Scripts/ColorMenu.cs
+++ Assets/Scripts/ColorMenu.cs
@@ -1,4 +1,7 @@
-﻿using TMPro;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -25,11 +28,47 @@ public class ColorMenu : MonoBehaviour {
 	[Space(15)]
 	public HexInput HexValue;
 
+	[Space(15)]
 	public Character character;
 
+	[Space(15)]
+	public GameObject ColorPresetMenu;
+	public GameObject ColorPresetList;
+	public GameObject ColorPresetButtonPrefab;
 
-	// Start is called before the first frame update
+	private ColorPresetsClass ColorPresets = new ColorPresetsClass();
+
+	private string path;
+
+	private void OnEnable() {
+		if (File.Exists(path)) {
+			StreamReader reader = new StreamReader(path);
+
+			string jsonData = reader.ReadToEnd();
+			ColorPresets = JsonUtility.FromJson<ColorPresetsClass>(jsonData);
+
+			reader.Close();
+		}
+	}
+
 	void Start() {
+		path = Application.persistentDataPath + "/ColorPresets/presets.json";
+
+		if (File.Exists(path)) {
+			StreamReader reader = new StreamReader(path);
+
+			string jsonData = reader.ReadToEnd();
+			ColorPresets = JsonUtility.FromJson<ColorPresetsClass>(jsonData);
+
+			reader.Close();
+		} else {
+			FileInfo file1 = new System.IO.FileInfo(Application.persistentDataPath + "/ColorPresets/");
+			file1.Directory.Create();
+
+			string jsonData = JsonUtility.ToJson(ColorPresets.presets, true);
+			File.WriteAllText(path, jsonData);
+		}
+
 		RValue.SetValue(OutColor.r);
 		GValue.SetValue(OutColor.g);
 		BValue.SetValue(OutColor.b);
@@ -43,10 +82,6 @@ public class ColorMenu : MonoBehaviour {
 		HexValue.SetValue(ColorUtility.ToHtmlStringRGB(OutColor));
 
 		UpdateColorRGB();
-	}
-
-	void Update() {
-
 	}
 
 	public void UpdateColorRGB(bool overrideMode = false) {
@@ -88,19 +123,14 @@ public class ColorMenu : MonoBehaviour {
 	}
 
 	public void UpdateColorHex() {
-		float H, S, V;
-		Color color = new Color();
 
-		color.r = System.Convert.ToInt32(HexValue.GetValue().Substring(0, 2), 16);
-		color.g = System.Convert.ToInt32(HexValue.GetValue().Substring(2, 2), 16);
-		color.b = System.Convert.ToInt32(HexValue.GetValue().Substring(4, 2), 16);
-		color.a = 255;
+		Color color = HexToColor(HexValue.GetValue());
 
 		RValue.SetValue(color.r);
 		GValue.SetValue(color.g);
 		BValue.SetValue(color.b);
 
-		Color.RGBToHSV(color, out H, out S, out V);
+		Color.RGBToHSV(color, out float H, out float S, out float V);
 		HValue.SetValue(H);
 		SValue.SetValue(S);
 		VValue.SetValue(V);
@@ -130,7 +160,68 @@ public class ColorMenu : MonoBehaviour {
 		BValue.SetValue(colorRGB.b);
 	}
 
+	public static Color HexToColor(string hexValue) {
+		return new Color { r = System.Convert.ToInt32(hexValue.Substring(0, 2), 16), g = System.Convert.ToInt32(hexValue.Substring(2, 2), 16), b = System.Convert.ToInt32(hexValue.Substring(4, 2), 16), a = 255 };
+	}
+
+	public void SavePreset() {
+		List<string> tempArray = new List<string>();
+
+		tempArray.AddRange(ColorPresets.presets);
+		tempArray.Add(HexValue.GetValue());
+
+		ColorPresets.presets = tempArray.ToArray();
+
+		string jsonData = JsonUtility.ToJson(ColorPresets, true);
+		File.WriteAllText(path, jsonData);
+	}
+
+	public void OpenPresetsMenu() {
+		foreach (string preset in ColorPresets.presets) {
+			GameObject colorPreset = Instantiate(ColorPresetButtonPrefab, ColorPresetList.transform);
+
+			colorPreset.GetComponent<ColorPresetButton>().HexValue = preset;
+			colorPreset.GetComponent<ColorPresetButton>().colorMenu = this;
+		}
+
+		ColorPresetMenu.SetActive(true);
+	}
+
+	public void ClosePresetsMenu() {
+		foreach(Transform menu in ColorPresetList.transform) {
+			Destroy(menu.gameObject);
+		}
+
+		ColorPresetMenu.SetActive(false);
+	}
+
+	public void LoadPreset(string preset) {
+		HexValue.SetValue(preset);
+
+		UpdateColorHex();
+
+		ClosePresetsMenu();
+	}
+
+	public void RemovePreset(string preset) {
+		foreach(Transform Menu in ColorPresetList.transform) {
+			if(Menu.name == preset) { Destroy(Menu.gameObject); break; }
+		}
+
+		List<string> tempArray = new List<string>();
+
+		tempArray.AddRange(ColorPresets.presets);
+		tempArray.Remove(preset);
+
+		ColorPresets.presets = tempArray.ToArray();
+	}
+
 	public void CloseMenu() {
 		character.CloseColorMenu(transform.GetSiblingIndex());
 	}
+}
+
+[Serializable]
+public class ColorPresetsClass {
+	public string[] presets = new string[0];
 }
---
+++ Assets/Scripts/MenuItems/ColorPresetButton.cs
@@ -0,0 +1,46 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPresetButton : MonoBehaviour {
+	public string HexValue;
+
+	private Button button;
+	private TMP_Text buttonText;
+	private Button removeButton;
+
+	private Color color;
+
+	public ColorMenu colorMenu;
+
+	void Start() {
+		button = transform.GetComponent<Button>();
+		buttonText = transform.GetComponentInChildren<TMP_Text>();
+		removeButton = transform.GetChild(1).GetComponent<Button>();
+
+		color = ColorMenu.HexToColor(HexValue);
+
+		Color32 temp2 = new Color32 { r = (byte)color.r, g = (byte)color.g, b = (byte)color.b, a = 255 };
+
+		button.GetComponent<Image>().color = temp2;
+
+		buttonText.text = HexValue;
+
+		gameObject.name = HexValue;
+
+		Color32 temp = new Color { r = Color.white.r - color.r, g = Color.white.g - color.g, b = Color.white.b - color.b, a = Color.white.a };
+		temp2 = new Color32 { r = (byte)temp.r, g = (byte)temp.g, b = (byte)temp.b, a = 255 };
+
+		buttonText.color = temp2;
+
+		removeButton.GetComponent<Image>().color = temp2;
+	}
+
+	public void LoadPreset () {
+		colorMenu.LoadPreset(HexValue);
+	}
+
+	public void RemovePreset() {
+		colorMenu.RemovePreset(HexValue);
+	}
+}
